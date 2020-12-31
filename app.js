897385ea7c30f78acc374d2c309885e644a3f82c
byun-sungwoo <dbyun1999@gmail.com>
2020-12-31 03:08:51 -0500

    quiz mode added

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
  * lolquiz - discord bot
  */
 
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const token = require("./token.js")
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = require("./token.js");
 const fetch = require("node-fetch");
 
 var championData = [];
@@ -64,13 +64,6 @@ class Champion {
 	}
 }
 
-class Question {
-	constructor(answer = '123') {
-		this.winner = null;
-		this.answer = answer;
-	}
-}
-
 /**
  * n			- number of questions
  * questions	- list of questions
@@ -79,37 +72,56 @@ class Question {
  */
 class Game {
 	constructor(n) {
-		if(n < 1) n = 1;
-		if(n > 10) n = 10;
-		this.n = n;
+		this.n = validRounds(n);
 		this.questions = [];
 		this.index = 0;
 		this.gameover = false;
-		initgame();
+		this.initgame();
 	}
 
 	initgame() {
 		for(let i = 0; i < this.n; i++) {
-			this.questions.push(new Question(`answer ${i}`));
+			let randChamp = Math.floor(Math.random()*championData.length);
+			this.questions.push(randChamp);
+			console.log(`PUSHING ${championData[randChamp].name}: ` + randChamp)
 		}
 	}
 
-	next() {
-		if(this.questions.length >= index) {
-			this.gameover = true;
-			console.log('> game over')
-		} else {
-			index++;
-			console.log(`> count increased to ${index}`)
+	isCorrect(ans) {
+		ans = ans.split(' ').join('');
+		ans = ans.split('\'').join('');
+		if(ans.toLowerCase() === this.getChampion().name.toLowerCase()) {
+			this.next();
+			return true;
 		}
+		return false;
+	}
+
+	getChampion() {
+		return !this.gameover ? championData[this.questions[this.index]] : null;
+	}
+
+	next() {
+		this.index++;
+		if(this.questions.length <= this.index)
+			this.gameover = true;
 	}
 }
 
 // ---------- ---------- ---------- ---------- ----------
 var mainGame = null;
+let mainIdx = 0;
+let sIdx = -1;
+let correct = false;
+
+function reset() {
+	mainIdx = 0;
+	sIdx = -1;
+	correct = false;
+}
 
 fetch(url)
-	.then(res => res.json())
+	.then(res => res.json())	// load data first
 	.then((out) => {
 		let champions = out.data;
 		for(var i in champions) {
@@ -117,19 +129,48 @@ fetch(url)
 			championData.push(new Champion(tmp.id, tmp.blurb, tmp.title, tmp.tags));
 		}
 	})
-	.then(() => {
+	.then(() => {	// then init
 		initBot()
 	}).catch(err => console.error(err));
 
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function validRounds(rounds) {
+	if(rounds < 1)
+		return 1;
+	if(rounds > 10)
+		return 10;
+	return rounds;
+}
+
 function initBot() {
 	client.on('ready', () => {
-		console.log(`Logged in as ${client.user.tag}.`)
-		client.user.setActivity('lol -help') 
+		console.log(`Logged in as ${client.user.tag}.`);
+		client.user.setActivity('lol -help');
 	})
 
 	client.on('message', msg => {
-		let embMessage = null;
 		if(msg.author.bot) return;
+		if(mainGame !== null && !mainGame.gameover) {
+			if(mainGame.isCorrect(msg.content)){
+				msg.react('👍');
+				if(!mainGame.gameover)
+					msg.channel.send(`${msg.author.username} got it!\nNext Question!`);
+				correct = true;
+			} else {
+				msg.delete();
+				// msg.react('🤣');
+				// msg.react('🤏');
+			}
+		}
+	})
+
+	client.on('message', msg => {
+		if(msg.author.bot) return;
+		let embMessage = null;
+		// console.log(`ID:${msg.author.id} "${msg.content}"`);
 		let sp = msg.content.split('lol ')[1];
 		let flag = false;
 		if(msg.content.startsWith("lol") && sp !== undefined && sp.length > 0) {
@@ -143,8 +184,11 @@ function initBot() {
 						{ name: 'General', value: `\`lol -[command]\nlol -[first character of command]\``},
 						{ name: 'Play', value: `\`lol -play [number of rounds]\``},
 						{ name: 'Lookup Champion', value: `\`lol -lookup [champion name]\``},
+						{ name: 'Start', value: `\`lol -start\``},
+						{ name: 'Cancel', value: `\`lol -cancel\``},
 					);
-				msg.channel.send(embMessage);
+				msg.author.send(embMessage);
+				msg.delete();
 				break;
 			case '-l':
 				if(input === undefined || input.length <= 1) {
@@ -162,7 +206,7 @@ function initBot() {
 					name = name.split('\'').join('');
 					for(var i in championData) {
 						if(championData[i].name.toLowerCase() === name.toLowerCase()) {
-							console.log(championData[i]);
+							// console.log(championData[i]);
 							embMessage = new Discord.MessageEmbed()
 								.setColor('#5BE6DB')
 								.setTitle(`${championData[i].name}`)
@@ -197,16 +241,95 @@ function initBot() {
 				} else {
 					let rounds = sp.replace(input[0], '');
 					if(rounds == parseInt(rounds, 10)) {
-						if(mainGame === null || mainGame === undefined) {
+						if(mainGame === null || mainGame === undefined || mainGame.gameover) {
+							let countdown = 5;
 							embMessage = new Discord.MessageEmbed()
-								.setColor('#61FF61')
+								.setColor('#FFB061')
 								.setTitle(`Starting New Game`)
 								.addFields(
-									{ name: 'Rounds:', value: `${rounds}`},
-									// { name: 'Cancel:', value: `\`lol -cancel\``},
-								);
-							msg.channel.send(embMessage);
-							// mainGame = new Game(parseInt(rounds, 10));
+									{ name: 'Rounds:', value: `${validRounds(rounds)}`},
+									{ name: 'Game Starts In:', value: `${countdown}s`},
+							);
+							msg.channel.send(embMessage).then((msg)=> {
+								(async() => {
+									for(let i = 0; i <= countdown; i++) {
+										embMessage = new Discord.MessageEmbed()
+											.setColor('#FFB061')
+											.setTitle(`Starting New Game`)
+											.addFields(
+												{ name: 'Rounds:', value: `${validRounds(rounds)}`},
+												{ name: 'Game Starts In:', value: `${countdown-i}s`},
+										);
+										await sleep(1000);
+										msg.edit(embMessage);
+									}
+									embMessage = new Discord.MessageEmbed()
+										.setColor('#61FF61')
+										.setTitle(`Game Started`)
+										.addFields(
+											{ name: 'Rounds:', value: `${validRounds(rounds)}`},
+									);
+									msg.edit(embMessage);
+									mainGame = new Game(parseInt(rounds, 10));
+									let sent;
+									while(!mainGame.gameover) {
+										for(let i = 0; i < 3; i++) {
+											if(correct) {
+												reset();
+												break;
+											}
+											if(i === 0 && sIdx < mainGame.index) {
+												sIdx++;
+												embMessage = new Discord.MessageEmbed()
+													.setColor('#FFB061')
+													.setTitle(`Question ${mainGame.index+1}/${mainGame.questions.length}`)
+													.addFields(
+														{ name: 'Hint:', value: `${i+1}/${3}`},
+														{ name: 'Title:', value: `${mainGame.getChampion().title}`},
+												);
+												if(mainIdx < 3)
+													sent = await msg.channel.send(embMessage);
+											} else {
+												if(i === 1) {
+													embMessage = new Discord.MessageEmbed()
+													.setColor('#FFB061')
+													.setTitle(`Question ${mainGame.index+1}/${mainGame.questions.length}`)
+													.addFields(
+														{ name: 'Hint:', value: `${i+1}/${3}`},
+														{ name: 'Title:', value: `${mainGame.getChampion().title}`},
+														{ name: 'Class:', value: `${mainGame.getChampion().tags}`},
+													);
+												} else {
+													embMessage = new Discord.MessageEmbed()
+													.setColor('#FFB061')
+													.setTitle(`Question ${mainGame.index+1}/${mainGame.questions.length}`)
+													.addFields(
+														{ name: 'Hint:', value: `${i+1}/${3}`},
+														{ name: 'Title:', value: `${mainGame.getChampion().title}`},
+														{ name: 'Class:', value: `${mainGame.getChampion().tags}`},
+														{ name: 'Lore:', value: `\`${mainGame.getChampion().blurbCensor}\``},
+													);
+												}
+												if(mainIdx < 3)
+													sent.edit(embMessage);
+											}
+											await sleep(10000);
+											mainIdx++;
+										}
+									}
+									if(mainGame !== null && mainGame.gameover) {
+										embMessage = new Discord.MessageEmbed()
+										.setColor('#61FF61')
+										.setTitle(`End Of Game`)
+										msg.channel.send(embMessage);
+									} else {
+										embMessage = new Discord.MessageEmbed()
+										.setColor('#FF6161')
+										.setTitle(`uh, this msg isn't supposed to show up...`)
+										msg.channel.send(embMessage);
+									}
+								})();
+							});
 						} else {
 							embMessage = new Discord.MessageEmbed()
 								.setColor('#FF6161')
